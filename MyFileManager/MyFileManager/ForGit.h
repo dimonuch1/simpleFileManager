68790ad02333b0299749e7678f0d7faea1a6c52a
dimonuch1 <dimonuch1@gmail.com>
2016-09-20 21:48:48 +0300

    add delete

---
+++ MyFileManager/MyFileManager/ForGit.h
@@ -0,0 +1,13 @@
+//
+//  ForGit.h
+//  MyFileManager
+//
+//  Created by Dima on 20.09.16.
+//  Copyright © 2016 Dima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ForGit : NSObject
+
+@end
